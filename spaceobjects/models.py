b2c48d9328e76161320f3a8accee5628d7bf00bb
Judy Mou <moujudy@gmail.com>
2018-12-01 23:25:05 -0800

    add one field in spaceobject

--- spaceobjects/models.py
+++ spaceobjects/models.py
@@ -2,5 +2,9 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib import admin
 
-# Create your models here.
+class SpaceObject(models.Model):
+  name = models.CharField(max_length=500)
+
+admin.site.register(SpaceObject)
