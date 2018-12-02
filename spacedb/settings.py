6cb31cef90604fc97947c3be450cc3630d6ae6d6
Judy Mou <moujudy@gmail.com>
2018-12-01 23:56:27 -0800

    refactor frontend templates

--- spacedb/settings.py
+++ spacedb/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'spacedb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+  os.path.join(BASE_DIR, 'static'),
+]
