f5942b012d1eb4f6d7b872186f10103f3b24ac41
Ian Webster <ianw_github@ianww.com>
2018-12-12 17:55:43 -0800

    Change asteroid detail route to /asteroid

--- spaceobjects/urls.py
+++ spaceobjects/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from .import views
 
 urlpatterns = [
-  url('(?P<name>[^/]+)$', views.detail, name='detail'),
+  url('^asteroid/(?P<name>[^/]+)$', views.detail, name='detail'),
   url('', views.index, name='index'),
 ]
