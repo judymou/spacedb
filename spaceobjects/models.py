dc405230053ede55a964f36fdefaf1b9be042795
Ian Webster <ianw_git@ianww.com>
2018-12-16 13:20:10 -0800

    Add SpaceObject index to make searching quicker

--- spaceobjects/models.py
+++ spaceobjects/models.py
@@ -10,98 +10,107 @@ from jsonfield import JSONField
 from .description import get_orbit_class, get_orbit_desc, get_diameter_comparison
 
 class SpaceObject(models.Model):
-  fullname = models.CharField(max_length=200)
-  name = models.CharField(max_length=200)
-  slug = models.CharField(max_length=200)
+    fullname = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    slug = models.CharField(max_length=200)
 
-  # Basic orbital elements
-  a = models.FloatField()
-  e = models.FloatField()
-  i = models.FloatField()
-  om = models.FloatField()
-  w = models.FloatField()
-  ma = models.FloatField()
-  epoch = models.FloatField()
+    # Basic orbital elements
+    a = models.FloatField()
+    e = models.FloatField()
+    i = models.FloatField()
+    om = models.FloatField()
+    w = models.FloatField()
+    ma = models.FloatField()
+    epoch = models.FloatField()
 
-  # sbdb blob
-  sbdb_entry = JSONField()
+    # sbdb blob
+    sbdb_entry = JSONField()
 
-  def get_absolute_url(self):
-      return '/asteroid/%s' % self.slug
+    class Meta:
+        indexes = [
+            models.Index(fields=['fullname'])
+        ]
 
-  def get_orbit_class(self):
-      return get_orbit_class(self)
+    def get_absolute_url(self):
+        return '/asteroid/%s' % self.slug
 
-  def get_orbit_desc(self):
-      return get_orbit_desc(self)
+    def get_orbit_class(self):
+        return get_orbit_class(self)
 
-  def is_neo(self):
-      entry = self.sbdb_entry.get('neo', False)
-      if entry == 'N':
-          return False
-      return entry
+    def get_orbit_desc(self):
+        return get_orbit_desc(self)
 
-  def is_pha(self):
-      entry = self.sbdb_entry.get('pha', False)
-      if entry == 'N':
-          return False
-      return entry
+    def is_neo(self):
+        entry = self.sbdb_entry.get('neo', False)
+        if entry == 'N':
+            return False
+        return entry
 
-  def get_discovery_date(self):
-      firstobs = self.sbdb_entry.get('first_obs')
-      return datetime.strptime(firstobs, '%Y-%m-%d')
+    def is_pha(self):
+        entry = self.sbdb_entry.get('pha', False)
+        if entry == 'N':
+            return False
+        return entry
 
-  def get_size_adjective(self):
-      diameter_str = self.sbdb_entry.get('diameter')
-      if not diameter_str:
-          return None
+    def get_discovery_date(self):
+        firstobs = self.sbdb_entry.get('first_obs')
+        return datetime.strptime(firstobs, '%Y-%m-%d')
 
-      diameter = float(diameter_str)
-      if diameter < 1:
-          return 'very small'
-      if diameter < 10:
-          return 'small'
-      if diameter < 100:
-          return 'relatively small'
-      if diameter < 200:
-          return 'average-sized'
-      if diameter < 300:
-          return 'large'
-      if diameter < 600:
-          return 'very large'
-      return 'dwarf planet'
+    def get_size_adjective(self):
+        diameter_str = self.sbdb_entry.get('diameter')
+        if not diameter_str:
+            return None
 
-  def has_size_info(self):
-      diam = self.sbdb_entry.get('diameter')
-      return diam != '' and diam is not None
+        diameter = float(diameter_str)
+        if diameter < 1:
+            return 'very small'
+        if diameter < 10:
+            return 'small'
+        if diameter < 100:
+            return 'relatively small'
+        if diameter < 200:
+            return 'average-sized'
+        if diameter < 300:
+            return 'large'
+        if diameter < 600:
+            return 'very large'
+        return 'dwarf planet'
 
-  def get_size_rough_comparison(self):
-      diameter_str = self.sbdb_entry.get('diameter')
-      if not diameter_str:
-          return None
+    def has_size_info(self):
+        diam = self.sbdb_entry.get('diameter')
+        return diam != '' and diam is not None
 
-      diameter = float(diameter_str)
-      if diameter > 900:
-          return 'the largest asteroid/dwarf planet'
-      if diameter > 300:
-          return 'one of the largest asteroids'
-      if diameter > 1:
-          return 'larger than most asteroids'
-      return 'a small to average asteroid'
+    def get_size_rough_comparison(self):
+        diameter_str = self.sbdb_entry.get('diameter')
+        if not diameter_str:
+            return None
 
-  def get_diameter_comparison(self):
-      return get_diameter_comparison(self)
+        diameter = float(diameter_str)
+        if diameter > 900:
+            return 'the largest asteroid/dwarf planet'
+        if diameter > 300:
+            return 'one of the largest asteroids'
+        if diameter > 1:
+            return 'larger than most asteroids'
+        return 'a small to average asteroid'
 
-  def get_similar_orbits(self, n=3):
-      a_range = [self.a - 0.01, self.a + 0.01]
-      similar = SpaceObject.objects.filter(a__range=a_range).exclude(pk=self.pk)
-      return similar[:n]
+    def get_diameter_comparison(self):
+        return get_diameter_comparison(self)
 
-  def to_search_result(self):
-      return {
-        'fullname': self.fullname,
-        'name': self.name,
-        'slug': self.slug,
-      }
+    def get_similar_orbits(self, n=3):
+        a_range = [self.a - 0.01, self.a + 0.01]
+        similar = SpaceObject.objects.filter(a__range=a_range).exclude(pk=self.pk)
+        return similar[:n]
+
+    def to_search_result(self):
+        return {
+          'fullname': self.fullname,
+          'name': self.name,
+          'slug': self.slug,
+        }
+
+class CloseApproach(models.Model):
+    space_object = models.ForeignKey(SpaceObject)
+    dist_min = models.FloatField()
 
 admin.site.register(SpaceObject)
--- spaceobjects/views.py
+++ spaceobjects/views.py
@@ -8,17 +8,17 @@ from django.shortcuts import render, redirect
 from .models import SpaceObject
 
 def index(request):
-  space_objects = SpaceObject.objects.all()
-  return render(request, 'spaceobjects/index.html',
-      {'space_objects': space_objects})
+    space_objects = SpaceObject.objects.all()[:30]
+    return render(request, 'spaceobjects/index.html',
+          {'space_objects': space_objects})
 
 def detail(request, slug):
-  try:
-    space_object = SpaceObject.objects.get(slug__iexact=slug)
-  except SpaceObject.DoesNotExist:
-    return index(request)
-  return render(request, 'spaceobjects/detail.html',
-      {'object': space_object})
+    try:
+        space_object = SpaceObject.objects.get(slug__iexact=slug)
+    except SpaceObject.DoesNotExist:
+        return index(request)
+    return render(request, 'spaceobjects/detail.html',
+        {'object': space_object})
 
 def search(request):
     search_str = request.GET.get('q')
