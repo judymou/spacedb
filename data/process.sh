cceedb9c14650878bcfe3683c526c3aa6c4f8cdd
Ian Webster <ianw_github@ianww.com>
2018-12-27 10:46:28 -0500

    Add catch-all process script

---
+++ data/process.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+pushd $(dirname $0) &>/dev/null
+
+./process_sbdb.py
+./process_close_approach.py
+./process_sentry.py
+./process_nhats.py
+
+popd &>/dev/null
--- data/run_pipeline.sh
+++ data/run_pipeline.sh
@@ -3,10 +3,7 @@
 pushd $(dirname $0) &>/dev/null
 
 ./download.sh
-./process_sbdb.py
-./process_close_approach.py
-./process_sentry.py
-./process_nhats.py
+./process.sh
 
 popd &>/dev/null
 
