273ef15fbe011d5aed403bee11700c3ed53dd159
Ian Webster <ianw_github@ianww.com>
2018-12-27 11:49:12 -0500

    Add docker boostrap script

---
+++ scripts/bootstrap_new_instance.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Run the initial migrations, creating the database
+docker-compose run --rm app /bin/sh -c "cd /app; ./manage.py migrate"
+
+# Run the pipeline
+docker-compose run --rm app /bin/bash -c "cd /app; ./data/run_pipeline.sh"
+
+# Create an admin login
+docker-compose run --rm app /bin/sh -c "cd /app; ./manage.py createsuperuser"
