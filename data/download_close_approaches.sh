38d80783eaaabef23295e3f0042b5c7d9ee4066b
Ian Webster <ianw_github@ianww.com>
2018-12-27 10:45:06 -0500

    fix close approach download location

--- data/download_close_approaches.sh
+++ data/download_close_approaches.sh
@@ -2,6 +2,6 @@
 
 pushd $(dirname $0) &>/dev/null
 
-curl 'https://ssd-api.jpl.nasa.gov/cad.api?date-min=2019-01-01&date-max=2200-01-01&fullname=true' -o close_approach.json
+curl 'https://ssd-api.jpl.nasa.gov/cad.api?date-min=2019-01-01&date-max=2200-01-01&fullname=true' -o rawdata/close_approach.json
 
 popd &>/dev/null
