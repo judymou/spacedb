dda4f6bcf40c6f5c74ad090c138f9c9c85887b90
Ian Webster <ianw_git@ianww.com>
2019-01-06 23:07:54 -0800

    bootstrap: load fixtures

--- scripts/bootstrap_new_instance.sh
+++ scripts/bootstrap_new_instance.sh
@@ -2,6 +2,7 @@
 
 # Run the initial migrations, creating the database
 docker-compose run --rm app /bin/sh -c "cd /app; ./manage.py migrate"
+docker-compose run --rm app /bin/sh -c "cd /app; ./manage.py loaddata orbit_class"
 
 # Run the pipeline
 docker-compose run --rm app /bin/bash -c "cd /app; ./data/run_pipeline.sh"
