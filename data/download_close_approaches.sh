eae955cae1678995bbafbf7e2259a898333cbaf9
Ian Webster <ianw_github@ianww.com>
2019-01-31 15:19:28 -0800

    Increase close approach distance threshold. Closes #25

--- data/download_close_approaches.sh
+++ data/download_close_approaches.sh
@@ -2,6 +2,6 @@
 
 pushd $(dirname $0) &>/dev/null
 
-curl 'https://ssd-api.jpl.nasa.gov/cad.api?date-min=2019-01-01&date-max=2200-01-01&fullname=true' -o rawdata/close_approach.json
+curl 'https://ssd-api.jpl.nasa.gov/cad.api?date-min=2019-01-01&date-max=2200-01-01&dist-max=0.5&fullname=true' -o rawdata/close_approach.json
 
 popd &>/dev/null
