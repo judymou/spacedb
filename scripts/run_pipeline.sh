79ee30b9b0d4929ea443c64e9df54caf37e1ea61
Ian Webster <ianw_git@ianww.com>
2019-08-31 10:46:38 -0700

    Time pipeline

--- scripts/run_pipeline.sh
+++ scripts/run_pipeline.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Run the pipeline
-docker-compose run --rm app /bin/bash -c "cd /app; ./data/run_pipeline.sh"
+time docker-compose run --rm app /bin/bash -c "cd /app; ./data/run_pipeline.sh"
