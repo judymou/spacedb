de6931bb237b0257a1789c21b34d57e7dc5bf23d
Ian Webster <ianw_github@ianww.com>
2020-01-11 11:40:14 +0300

    Pull in M1

--- data/download_sbdb.sh
+++ data/download_sbdb.sh
@@ -2,7 +2,7 @@
 
 pushd $(dirname $0) &>/dev/null
 
-curl -v 'https://ssd-api.jpl.nasa.gov/sbdb_query.api?fields=full_name,pdes,name,class,neo,pha,moid,moid_jup,epoch,e,a,q,i,om,w,ma,tp,per,n,ad,first_obs,last_obs,n_obs_used,H,diameter,density,extent,rot_per,GM,pole,albedo,BV,UB,IR,spec_T,spec_B' -o rawdata/sbdb.json
+curl -v 'https://ssd-api.jpl.nasa.gov/sbdb_query.api?fields=full_name,pdes,name,class,neo,pha,moid,moid_jup,epoch,e,a,q,i,om,w,ma,tp,per,n,ad,first_obs,last_obs,n_obs_used,H,M1,diameter,density,extent,rot_per,GM,pole,albedo,BV,UB,IR,spec_T,spec_B' -o rawdata/sbdb.json
 gzip -f rawdata/sbdb.json
 
 popd &>/dev/null
