2e201715e36b64462d4aaa8571d599f975d61582
Ian Webster <ianw_github@ianww.com>
2021-06-04 07:17:26 -0700

    temporarily use old damit datasource

--- data/download_damit.sh
+++ data/download_damit.sh
@@ -8,7 +8,8 @@ cd rawdata/shapes
 
 # Download complete flush from
 # http://astro.troja.mff.cuni.cz/projects/asteroids3D/web.php?page=db_export
-curl -o damit.tar.gz 'https://astro.troja.mff.cuni.cz/projects/damit/exports/complete/damit-20210401T000301Z.tar.gz'
+#curl -o damit.tar.gz 'https://astro.troja.mff.cuni.cz/projects/damit/exports/complete/damit-20210401T000301Z.tar.gz'
+curl -o damit.tar.gz 'https://www.ianww.com/damit_old.tar.gz'
 
 # Decompress
 tar xzvf damit.tar.gz
